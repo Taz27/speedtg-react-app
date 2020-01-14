bb7b57dd6a933bf4f9ff88cb984be7ba9ddc89d7
Taran Mand <tsmand@yahoo.com>
2020-01-14 20:01:14 +1100

    Render Props Practice

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import App from './App';
+import RenderPropApp from './components/RenderPropApp';
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<RenderPropApp />, document.getElementById("root"));
 
 
 
