de9c5595c158c2af518c9007fca7c548c9e81e27
Taran Mand <tsmand@yahoo.com>
2020-01-24 17:37:36 +1100

    Add custom hook

--- src/App.js
+++ src/App.js
@@ -1,56 +1,9 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React from 'react';
+import useWordGame from './HOOKS/useWordGame';
 
 function App() {
-    const START_TIME = 15;
-    
-    const [text, setText] = useState("");
-    const [timeRemaining, setTimeRemaining] = useState(START_TIME);
-    const [isTimeRunning, setIsTimeRunning] = useState(false);
-    const [wordCount, setWordCount] = useState(0);
-    
-    const textAreaRef = useRef(null); 
 
-    function handleChange(event) {
-        const {value} = event.target;
-        setText(value);
-    }
-
-    function calculateWordCount(txt) {
-        const wordsArray = txt.trim().split(/\s+/);
-        const wordCount = wordsArray[0] === "" ? 0 : wordsArray.length;
-        return wordCount;
-    }
-
-    useEffect(() => {
-        //decrement time remaining every second
-        if (isTimeRunning && timeRemaining > 0) {
-            
-            setTimeout(() => {
-                setTimeRemaining(prevTR => prevTR - 1);
-            }, 1000);
-        } else if (timeRemaining === 0) {
-            endGame();
-        }
-        
-    }, [timeRemaining, isTimeRunning]);
-
-    function startGame() {
-        //reset game state
-        setText("");
-        setWordCount(0);
-        setTimeRemaining(START_TIME);
-        setIsTimeRunning(true);
-        //put focus on text box 
-        textAreaRef.current.disabled = false;
-        textAreaRef.current.focus();
-    }
-
-    function endGame() {
-        //show word count and set isTimeRunning flag to false
-        setWordCount(calculateWordCount(text));
-        setIsTimeRunning(false);
-    }
-
+    const {text, textAreaRef, handleChange, isTimeRunning, timeRemaining, startGame, wordCount} = useWordGame(15);
 
     return (
         <div>
